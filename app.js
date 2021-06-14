3f6fcd9790822414bdf89582703101169bdfd126
Saleh Zarei <saleh.zarei@gmail.com>
2021-06-14 19:29:42 +0430

    تغییرات جدید 23 خرداد

--- app.js
+++ app.js
@@ -5,10 +5,10 @@ const app = express()
 const usersRouter = require('./api/users/users.router')
 
 app.use(express.json())
-app.use(express.urlencoded({extended:false}))
+app.use(express.urlencoded({ extended: false }))
 
 app.use('/', usersRouter)
 
-app.listen(process.env.APP_PORT , ()=>{
-    console.log("Server UP and Run in Port : " , process.env.APP_PORT)
+app.listen(process.env.APP_PORT, () => {
+    console.log("Server UP and Run in Port : ", process.env.APP_PORT)
 })
