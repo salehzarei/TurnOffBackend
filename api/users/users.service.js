0eb3c8c1d7e2f5c431b515ceea1207cdabeb080d
Saleh Zarei <saleh.zarei@gmail.com>
2021-06-15 15:34:58 +0430

    تغییرات جدید و ایجاد چند API

--- api/users/users.service.js
+++ api/users/users.service.js
@@ -6,7 +6,7 @@ module.exports = {
 
     getUserByPhoneNumber: (phone, callback) => {
         pool.query('SELECT * FROM `users` WHERE `userphone` = ?', [phone], (err, result, fields) => {
-            
+
             if (err) {
                 return callback(err)
             }
@@ -39,5 +39,45 @@ module.exports = {
         }
         )
     },
+
+    addNewOTP: (data, callback) => {
+        // اضافه کردن کد احراز هویت و شماره تماس در جدول
+        pool.query('INSERT INTO `otp` (`phone`, `otp`) VALUES (?,?)', [data.phone, data.otp], (err, result, fields) => {
+            if (err) { return callback(err) } return callback(null, result)
+        })
+    },
+
+    checkOTP: (data, callback) => {
+        // جستجو کد احراز هویت و شماره تماس در جدول
+        pool.query('SELECT * FROM `otp` WHERE `phone` LIKE ? AND `otp` LIKE ?', [data.phone, data.otp], (err, result, fields) => {
+            if (err) {
+                return callback(err)
+            } return callback(null, result)
+        })
+    },
+
+    updateUserData: (data, callback) => {
+        // به روز رسانی اطلاعات کاربری
+        var notetype = JSON.stringify(data.notetype);
+        var selectedcompany = JSON.stringify(data.selectedcompany)
+        var addresses = JSON.stringify(data.addresses)
+        pool.query('UPDATE `users` SET `userphone` = ?, `status` = ?, `notetype` = ?, `selectedcompany` = ?, `charge` = ? , `remindtime` = ?  , `addresses`= ?  WHERE `userToken` = ?', [
+            data.userphone,
+            data.status,
+            notetype,
+            selectedcompany,
+            data.charge,
+            data.remindtime,
+            addresses,
+            data.turnofftoken
+        ], (error, result, fields) => {
+            if (error) {
+                return callback(error)
+            }
+            return callback(null, result)
+        })
+    }
+
+
 
 }
