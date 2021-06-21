55d581cbe80e1225145a61beee1f2867e3222947
Saleh Zarei <saleh.zarei@gmail.com>
2021-06-21 19:52:31 +0430

    تغییرات در 31 خرداد

--- functions/sendnotification.js
+++ functions/sendnotification.js
@@ -24,29 +24,68 @@ module.exports = {
             if (err) {
                 console.log("Something has gone wrong!", err)
             } else {
-                console.log("Successfully sent with response: ", response)
-               return res.json({
-                success: 1,
-                messeage: "پیام با موفقیت ارسال شد",
-                data : response
-               })
+                console.log("ارسال انجام شده با پاسخ از سمت سرور: ", response)
+
+                if (response.failureCount == 1)
+                    return res.json({
+                        success: 0,
+                        messeage: "پیام به درستی ارسال نشد",
+                        data: response
+                    })
+                if (response.failureCount == 0)
+                    return res.json({
+                        success: 1,
+                        messeage: "پیام با موفقیت ارسال شده",
+                        data: response
+                    })
+
             }
         })
-        // var payloadMulticast = {
-        //     registration_ids:["4564654654654654",
-        //         '123123123',
-        //         'dBszBaOrRDWpVNcSrMxq-m:APA91bF9u-Dx-t3gbzNra6ZDtQpRW7u-m_j046zzCvlCcm4YxHfn9_lBvPbcGGcTMep9S7eC3KvJzK3szucpVnn9UtlPTEWUK2g10xYRF_z3rF01hwsqs6Y7VIqNoGrYYGy8DJ6O0LHl', //valid token among invalid tokens to see the error and ok response''
-        //         '123133213123123'],
-        //     data: {
-        //         url: "news"
-        //     },
-        //     priority: 'high',
-        //     content_available: true,
-        //     notification: { title: 'Hello', body: 'Multicast', sound : "default", badge: "1" }
-        // };
-        // fcm.send(payloadMulticast,function(err,res){
-        //     callbackLog('sendMulticast',err,res);
-        // });
-
     },
+    sendCastNotification: (req, res) => {
+        const body = req.body
+        console.log(body.registration_ids)
+        var payloadMulticast = {
+
+            registration_ids: body.registration_ids,
+            // registration_ids: [
+            //     'dBszBaOrRDWpVNcSrMxq-m:APA91bF9u-Dx-t3gbzNra6ZDtQpRW7u-m_j046zzCvlCcm4YxHfn9_lBvPbcGGcTMep9S7eC3KvJzK3szucpVnn9UtlPTEWUK2g10xYRF_z3rF01hwsqs6Y7VIqNoGrYYGy8DJ6O0LHl', //valid token among invalid tokens to see the error and ok response''
+            // ],
+            data: {
+                url: "news"
+            },
+            // priority: 'high',
+            //content_available: true,
+            notification: { title: 'اعلان قطعی برق', body: 'سلام براساس برنامه امروز ساعت 14:30 به مدیت یک ساعت برق محله شما قطع خواهد شد', sound: "default", badge: "1" }
+        };
+        fcm.send(payloadMulticast, function (err, response) {
+            //  callbackLog('sendMulticast', err, res);
+            console.log(response)
+            if (err) {
+                console.log(err)
+            } else {
+                if (response.failureCount == 1) {
+                     res.json({
+                        success: 0,
+                        messeage: "پیام به درستی ارسال نشد",
+                        data: response
+                    })
+                   
+
+                }
+
+                if (response.failureCount == 0)
+                     res.json({
+                        success: 1,
+                        messeage: "پیام با موفقیت ارسال شده",
+                        data: response
+                    })
+
+            }
+
+
+
+        });
+
+    }
 }
